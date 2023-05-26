ead815580f6e8429572540bf5421f58ff0334dfb
Ojidaemo <vit.martsinovich@gmail.com>
2023-05-26 13:33:03 +0300

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Vitali Martsinovich on 2023-05-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
